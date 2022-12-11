ab874a883592867a841f19f43b5e685f54646a13
Drennann <delpan.drennan@gmail.com>
2022-12-11 03:15:59 -0300

    add ProfileCard Component

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,22 +1,14 @@
 import Logo from "../svgs/Mercury-logotype.svg";
 import logout from "../svgs/i24-logout.svg";
 import { LoginProps } from "../interfaces/interfaces";
+import ProfileCard from "../components/ProfileCard";
 
 export default function Profile({ user, setUser }: LoginProps) {
   return (
     <div className="ProfileScreen">
       <img src={Logo} alt="Logo"></img>
       <div className="profileContainer">
-        {user?.data && (
-          <>
-            <img src={"./"+user.data.avatar} width={80} height={80} alt="avatar"></img>
-            <p>That's it, {user.data.name}!</p>
-            <button className="LoginButton" onClick={() => setUser({})}>
-              <img src={logout} alt="logout"></img>
-              <p>Logout</p>
-            </button>
-          </>
-        )}
+        <ProfileCard user={user} setUser={setUser}/>
       </div>
     </div>
   );
